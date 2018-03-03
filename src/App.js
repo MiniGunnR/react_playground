2e550b6ce154c340be94a34842e96d771e6db9da
MiniGunnR <hasan.mohaiminul@gmail.com>
2018-03-03 16:11:31 +0600

    added react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
 import Appbar from 'muicss/lib/react/appbar';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 import Item from './Item.js';
+
+const items = [
+  {
+    id: 1,
+    name: 'Item One',
+    quantity: 1,
+    unit: 'kg',
+    price: 50,
+  },
+  {
+    id: 2,
+    name: 'Item Two',
+    quantity: 1,
+    unit: 'dozen',
+    price: 100,
+  },
+  {
+    id: 3,
+    name: 'Item Three',
+    quantity: 1,
+    unit: 'dozen',
+    price: 75,
+  },
+]
 
 
 class SideLink extends Component {
@@ -25,30 +50,6 @@ class App extends Component {
   state = {cart: {}, total_items: 0, total_price: 0}
   render() {
     console.log(this.state.cart, this.state.total_price)
-
-    const items = [
-      {
-        id: 1,
-        name: 'Item One',
-        quantity: 1,
-        unit: 'kg',
-        price: 50,
-      },
-      {
-        id: 2,
-        name: 'Item Two',
-        quantity: 1,
-        unit: 'dozen',
-        price: 100,
-      },
-      {
-        id: 3,
-        name: 'Item Three',
-        quantity: 1,
-        unit: 'dozen',
-        price: 75,
-      },
-    ]
 
     const handleDecreaseClick = (id, price) => (e) => {
       let cart = {...this.state.cart}
@@ -94,7 +95,6 @@ class App extends Component {
     }
 
     const smallCartStyle = {
-      // backgroundColor: 'lightgreen',
       height: 75,
       width: 75,
       position: 'fixed',
@@ -125,44 +125,69 @@ class App extends Component {
     }
 
     return (
-      <div>
-        <Appbar>
-          <table width="100%">
-           <tbody>
-             <tr>
-               <td className="mui--appbar-height">Left Side</td>
-               <td className="mui--appbar-height" style={{textAlign: 'right'}}>Right Side</td>
-             </tr>
-           </tbody>
-          </table>
-        </Appbar>
+      <Router>
+        <div>
+          <Appbar>
+            <table width="100%">
+             <tbody>
+               <tr>
+                 <td className="mui--appbar-height">Left Side</td>
+                 <td className="mui--appbar-height" style={{textAlign: 'right'}}>Right Side</td>
+               </tr>
+             </tbody>
+            </table>
+          </Appbar>
 
-        <div style={ sidebarStyle }>
-          <SideLink name="KG"/>
-          <SideLink name="Dozen"/>
-          <SideLink name="All"/>
-        </div>
+          <div style={ sidebarStyle }>
+            {/* <SideLink name="KG"/>
+            <SideLink name="Dozen"/>
+            <SideLink name="All"/> */}
+            <Link to='/kg/'>
+              <p>KG</p>
+            </Link>
+            <Link to='/dozen/'>
+              <p>Dozen</p>
+            </Link>
+            <Link to='/all/'>
+              <p>All</p>
+            </Link>
+          </div>
 
-        {comps}
-
-        <div
-          id="small_cart"
-          style={ smallCartStyle }>
-            <div
-              id="small_cart_top"
-              style={ smallCartTopStyle}>
-                ICON <br/>
-                {this.state.total_items} ITEMS
+          <Route exact={true} path='/' render={() => (
+            <div>
+              {comps}
             </div>
-            <div
-              id="small_cart_bottom"
-              style={ smallCartBottomStyle }>
-              ৳ {this.state.total_price}
-            </div>
+          )}/>
+
+          <Route path='/all/' component={Comps} />
+
+          <div
+            id="small_cart"
+            style={ smallCartStyle }>
+              <div
+                id="small_cart_top"
+                style={ smallCartTopStyle}>
+                  ICON <br/>
+                  {this.state.total_items} ITEMS
+              </div>
+              <div
+                id="small_cart_bottom"
+                style={ smallCartBottomStyle }>
+                ৳ {this.state.total_price}
+              </div>
+          </div>
         </div>
-      </div>
+      </Router>
     )
   }
+}
+
+function Comps() {
+  return (
+    <div>
+      All items.
+    </div>
+  )
 }
 
 export default App;
